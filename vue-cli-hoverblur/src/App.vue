2d720b32aaa3707c6c49385bcd545b1ed912aea2
djdeo <32230775+djdeo@users.noreply.github.com>
2018-05-27 11:22:54 +0800

    hoverblur components

---
+++ vue-cli-hoverblur/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>{{ descrip }}</p>
+    <Hoverblur url="../static/slogo.png" enterImg="../static/rev.png" />
+  </div>
+</template>
+
+<script>
+import Hoverblur from './Hoverblur'
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Blogs,
+    Hoverblur
+  },
+  data () {
+    return {
+      title: 'no support local img, but online img works fine',
+      descrip:'see more info here: https://github.com/vuejs/Discussion/issues/202'
+    }
+  }
+}
+</script>
+
+<style>
+h1, p{
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
---
+++ vue-cli-hoverblur/src/Hoverblur.vue
@@ -0,0 +1,26 @@
+<template>
+    <figure class="hoverblur">
+        <img :src="imgSrc"  @mouseenter="imgSrc=enterImg"  @mouseleave="imgSrc=url">
+    </figure>
+</template>
+
+<script>
+export default {
+    name:'Hoverblur',
+    props:['url', 'enterImg'],
+    data(){
+        return{
+            imgSrc: this.url
+        }
+    }
+}
+</script>
+
+<style>
+.hoverblur{
+    max-width: 960px;
+    margin: 0 auto;
+}
+</style>
+
+
