84748ef38c62d833ba8a73ef76b568171eaa279c
djdeo <32230775+djdeo@users.noreply.github.com>
2018-05-30 22:41:40 +0800

    NavBar

---
+++ vue-blog/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-dark bg-warning mb-3">
+      <div class="container">
+        <a href="/" class="navbar-brand text-white">Server Articles</a>
+        <router-link :to=" {name: 'AddNew'} " class="btn btn-default">Add New</router-link>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NarBar'
+}
+</script>
+
