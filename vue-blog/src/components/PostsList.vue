9175448bb878d1b2b4094f7fc336f78383b57345
djdeo <32230775+djdeo@users.noreply.github.com>
2018-05-30 22:40:37 +0800

    Add deletePost()

--- vue-blog/src/components/PostsList.vue
+++ vue-blog/src/components/PostsList.vue
@@ -7,10 +7,13 @@
                 <div class="card-body">
                 <h4 class="card-title"> {{post.title}} </h4>
                 <p class="card-text"> {{post.body}} </p>
+                <router-link :to=" { name: 'Edit', params: { id: post.id, title: post.title, body:post.body }}" class="edit btn btn-sm btn-warning">Edit</router-link>
+                <button class="del btn btn-sm btn-danger" @click="deletePost(post.id)">Delete</button>
                 </div>
             </div>
         </div>
     </div>
+        <p class="text-success text-center">{{ feedback }}</p>
     </div>
     
 </template>
@@ -22,7 +25,8 @@ export default {
     name: 'PostsList',
     data(){
         return{
-            posts:[]
+            posts:[],
+            feedback:null
         }
     },
     created(){
@@ -33,8 +37,28 @@ export default {
             axios.get('http://localhost:3000/posts')
             .then(data => (this.posts = data.data))
             .catch(error => console.log(error))
+        },
+        deletePost(id){
+            let confirmDel = confirm(`Delete Post ${id}?`)
+            if (confirmDel) {
+                axios.delete(`http://localhost:3000/posts/${id}`)
+                .then(data => {
+                    this.feedback = `Post ${id} successfully deleted`
+                    this.getPost()
+                    })
+            }
         }
     }
 }
 </script>
+
+<style>
+.edit, .del {
+    display: none;
+}
+.card-body:hover .edit, .card-body:hover .del {
+    display: inline-block;
+}
+</style>
+
 
