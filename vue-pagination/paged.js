35af94e507f59afd3f6136a3bd2b039227a19fe0
djdeo <32230775+djdeo@users.noreply.github.com>
2018-08-28 13:20:21 +0800

    get json data

--- vue-pagination/paged.js
+++ vue-pagination/paged.js
@@ -1,28 +1,29 @@
 var vm = new Vue({
-    el: '#paged',
-    data: {
-        current: 0,
-        originalList: [
-            {title: 'title 1', body: 'body 1'},
-            {title: 'title 2', body: 'body 2'},
-            {title: 'title 3', body: 'body 3'},
-            {title: 'title 4', body: 'body 4'},
-            {title: 'title 5', body: 'body 5'},
-            {title: 'title 6', body: 'body 6'},
-            {title: 'title 7', body: 'body 7'}
-        ],
-        itemNum: 3
+  el: "#paged",
+  data: {
+    current: 0,
+    originalList: [],
+    rows: 3
+  },
+  created: function() {
+    var e = new XMLHttpRequest();
+    e.open("GET", "fakedata.json");
+    e.onload = function() {
+      var data = JSON.parse(this.response);
+      vm.originalList = data;
+    };
+    e.send();
+  },
+  computed: {
+    pageCount: function() {
+      let length = this.originalList.length,
+        size = this.rows;
+      return Math.floor(length / size);
     },
-    computed:{
-        pageCount: function() {
-            let length = this.originalList.length,
-                size = this.itemNum
-            return Math.floor(length / size)
-        },
-        pagedData: function() {
-            const start = this.current * this.itemNum
-                end = start + this.itemNum
-            return this.originalList.slice(start, end)
-        }
+    pagedData: function() {
+      const start = this.current * this.rows;
+      end = start + this.rows;
+      return this.originalList.slice(start, end);
     }
-})
+  }
+});
