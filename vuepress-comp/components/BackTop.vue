a02e317db928645f79034d2f4bde6aff97645efa
djdeo <32230775+djdeo@users.noreply.github.com>
2018-08-29 13:33:42 +0800

    back to top comp

---
+++ vuepress-comp/components/BackTop.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-if="scrolled">
+        <a href="#" class="top" title="Back to Top">&#8673;</a>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrolled: false
+    };
+  },
+  created() {
+    window.addEventListener("scroll", this.showLink);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.showLink);
+  },
+  methods: {
+    showLink() {
+      this.scrolled = window.scrollY > 0;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.top {
+  position: fixed;
+  top: 90%;
+  right: 100px;
+  opacity: 0.5;
+  z-index: 3;
+  font-size: 2rem;
+}
+</style>
